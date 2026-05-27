6c891109d0060050e230e37dfecaa2c6d4d2d809
Garth Trickett <garthtrickett@gmail.com>
2026-05-27 18:12:47 +1000

    Add Playwright e2e global-setup and a Web Test Runner smoke test file to fix test run failures.

---
+++ src/test/smoke.wtr.test.ts
@@ -0,0 +1,7 @@
+import { expect } from "@open-wc/testing";
+
+describe("Web Test Runner Smoke Test", () => {
+  it("should successfully run in the browser", () => {
+    expect(true).to.be.true;
+  });
+});
